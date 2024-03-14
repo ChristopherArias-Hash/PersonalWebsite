4c01089eeadfbd4f2dbb1ada52e5757f0d684d62
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-03-14 03:09:40 -0700

    FINALLY DONE OH MY LORD

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index={<Home />} />
+      <Route index element={<Home />} />
         <Route path="/Projects" element={<Projects />} />
         <Route path="/projectapp" element={<ProjectApp />} />
       </Routes>
