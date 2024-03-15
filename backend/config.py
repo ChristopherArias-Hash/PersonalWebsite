595910709547012202457e7082771ae2d11cbe95
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-03-15 01:55:18 -0700

    maybe will work now

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins=["https://christopher-arias.com"])
-
 # Storing a file which is in a SQLite database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
