b04022e8bbc4eb91c9615ff9141390370e474231
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-03-15 01:52:29 -0700

    test again

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://christopher-arias.com"])
 
 # Storing a file which is in a SQLite database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
