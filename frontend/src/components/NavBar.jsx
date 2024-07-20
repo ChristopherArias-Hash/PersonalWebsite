f9e82ce5690c79bec10d298198fa077cb294fb36
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-07-20 00:36:37 -0700

    got rid of unnessary maps

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,25 +5,21 @@ const Navbar = ({ navbar }) => {
   return (
     <div>
       {navbar.map((item) => (
-        <h1 className="welcomeSign" key={item.id}>{item.welcome}</h1>
+        <div key={item.id}>
+          <h1 className="welcomeSign">{item.welcome}</h1>
+          <div>
+            <Link to="/" className="navbuttons">
+              {item.home}
+            </Link>
+            <Link to="/Projects" className="navbuttons">
+              {item.projects}
+            </Link>
+            <a href="https://github.com/ChristopherArias-Hash" target="_blank" rel="noopener noreferrer" className="navbuttons">
+              {item.gitHub}
+            </a>
+          </div>
+        </div>
       ))}
-      <div>
-        {navbar.map((item) => (
-          <Link to="/" className="navbuttons" key={item.id}>
-            {item.home}
-          </Link>
-        ))}
-        {navbar.map((item) => (
-          <Link to="/Projects" className="navbuttons" key={item.id}>
-            {item.projects}
-          </Link>
-        ))}
-        {navbar.map((item) => (
-          <a href="https://github.com/ChristopherArias-Hash" target="_blank" className="navbuttons" key={item.id}>
-            {item.gitHub}
-          </a>
-        ))}
-      </div>
     </div>
   );
 };
