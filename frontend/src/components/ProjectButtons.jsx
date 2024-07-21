b13e95e2d6c5a862d184bb59ce100ec0825b30b6
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-07-21 00:24:33 -0700

    Made project section work on smaller screens

--- frontend/src/components/ProjectButtons.jsx
+++ frontend/src/components/ProjectButtons.jsx
@@ -2,15 +2,20 @@
 import React from "react";
 
 const ProjectButtons = ({ projectbuttons }) => {
-    return (
-        <div className="project-buttons-container">
-            {projectbuttons.map((item) => (
-                <a href={`https://github.com/ChristopherArias-Hash/${item.link}`} target="_blank" className="project-buttons" key={item.id}>            
-                    {item.buttonText}
-                </a>
-            ))}
-        </div>
-    );
+  return (
+    <div className="project-buttons-container">
+      {projectbuttons.map((item) => (
+        <a
+          href={`https://github.com/ChristopherArias-Hash/${item.link}`}
+          target="_blank"
+          className="project-buttons"
+          key={item.id}
+        >
+          {item.buttonText}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default ProjectButtons;
